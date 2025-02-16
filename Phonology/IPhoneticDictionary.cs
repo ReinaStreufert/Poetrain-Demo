319635f3802a9cfb424fdd7de4cc4aa6b8b7dbdd
ReinaStreufert <49079389+ReinaStreufert@users.noreply.github.com>
2025-02-16 14:33:47 -0500

    AAAAAAA

--- Phonology/IPhoneticDictionary.cs
+++ Phonology/IPhoneticDictionary.cs
@@ -240,11 +240,11 @@ namespace poetrain.Phonology
         {
             var basicScore = (a.EndConsonant == null || b.EndConsonant == null) ? 0.5f :
                 a.EndConsonant.ScoreRhyme(b.EndConsonant);
-            var aConsonantShiftScore = a.EndConsonant != null && b.BeginConsonants.Length > 0 ? 0f :
+            var aConsonantShiftScore = a.EndConsonant == null || b.BeginConsonants.Length == 0 ? 0f :
                 b.BeginConsonants
                 .Select(a.EndConsonant.ScoreRhyme)
                 .Max();
-            var bConsonantShiftScore = b.EndConsonant != null && a.BeginConsonants.Length > 0 ? 0f :
+            var bConsonantShiftScore = b.EndConsonant == null || a.BeginConsonants.Length == 0 ? 0f :
                 a.BeginConsonants
                 .Select(b.EndConsonant.ScoreRhyme)
                 .Max();
